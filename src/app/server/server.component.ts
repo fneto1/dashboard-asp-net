f272dbd8a42b7f2efb5cd14a695070b47e656441
fneto1 <fasneto2@gmail.com>
2023-07-25 11:57:46 -0300

    Atualização do componente server

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Server } from '../shared/server';
 
 @Component({
@@ -8,4 +8,27 @@ import { Server } from '../shared/server';
 })
 export class ServerComponent {
   @Input() serverInput!: Server;
+
+  color!: string;
+  buttonText!: string;
+
+  ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66bb6a';
+      this.buttonText = 'Shut down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#ff6b6b';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    this.setServerStatus(!onlineStatus);
+  }
 }
