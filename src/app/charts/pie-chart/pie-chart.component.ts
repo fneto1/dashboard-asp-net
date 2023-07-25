500ecaead5c688f7dfee1aba83a34ba7b895d19f
fneto1 <fasneto2@gmail.com>
2023-07-24 22:25:02 -0300

    Adição dos gráficos doughnut

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.css']
+  styleUrls: ['./pie-chart.component.css'],
 })
 export class PieChartComponent {
+  public pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+    },
+    borderColor: '#111',
+  };
 
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: ['Cobra', 'Wandão', 'DonPollo'],
+    datasets: [
+      {
+        data: [300, 500, 100],
+      },
+    ],
+  };
+  pieChartLabels: string[] = ['Cobra', 'Wandão', 'DonPollo'];
+  public pieChartType: ChartType = 'doughnut';
 }
